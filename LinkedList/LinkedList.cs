d71653d97a89881d25b063a16043f502ce9f7d92
shubhampachghare <shubhampachghare@gmail.com>
2023-03-24 01:25:33 +0530

    [Shubham] -SimpleLinkedList

---
+++ LinkedList/LinkedList.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    public class LinkedList
+    {
+        internal node head;
+        public void Add(int id)
+        {
+            node node1 = new node(id);
+            if (this.head == null)
+            {
+                this.head = node1;
+            }
+            else
+            {
+                node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                    temp.next = node1;
+                }
+
+            }
+            Console.WriteLine("{0} is inserted int linked list ", node1.id);
+        }
+    }
+}
---
+++ LinkedList/Node.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    internal class node
+    {
+        public int id;
+        public node next;
+        public node(int id)
+        {
+            this.id = id;
+        }
+    }
+}
